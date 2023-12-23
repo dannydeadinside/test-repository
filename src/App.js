b7476ef96273c94a48f803d40c8a1ba6e201de31
dannydeadinside <Danil.nagibin@bk.ru>
2023-12-23 17:19:55 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Это тестовый проект Нагибин Д.В.";
 
   console.log('This is App ');
   return (
